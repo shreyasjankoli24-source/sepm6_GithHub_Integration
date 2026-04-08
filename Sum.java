bc9f6cf1cf05e00678fb96ed7d4795e4030c7cd5
Shreyas Nilesh Jankoli <shreyas.jankoli24@ds.sce.edu.in>
2026-04-08 21:08:16 +0530

    Add Sum.java to calculate and display sum
    sum.java program for sepm practical 6

---
+++ Sum.java
@@ -0,0 +1,14 @@
+
+public class Sum{
+    
+    public static void main(String[] args) {
+   
+        System.out.println("Hello, World!");
+        
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
